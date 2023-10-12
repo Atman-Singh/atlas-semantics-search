70056dee43f894a51b2c76ee8a7e34a2a222c411
Atman Singh <39840838+Atman-Singh@users.noreply.github.com>
2023-10-12 00:31:52 -0400

    Vectorization algorithm sped up significantly

--- semantic_search.py
+++ semantic_search.py
@@ -9,7 +9,6 @@ def main():
     directory = r'C:\Users\Atman S\Documents\GitHub\embeddings\embeddings\sources\\'
 
     # list that will contain text from each page
-    vector_map = {}
     pages = []
     unique_words = []
 
@@ -22,13 +21,11 @@ def main():
                 unique_words.append(word)
     
     # convert words to vectors
-    vectors = vectorize_words(unique_words, pages)
+    vector_map = vectorize_words(unique_words, pages)
 
-    # create a hashmap using unique words as a key and the corresponding vector as its data entry
-    vector_map = create_hashmap(unique_words, vectors)
-
-    ui = input('Enter an FTC related word (eg. 3d): ')
-    print(semantics_search(vector_map, ui))
+    while True:
+        ui = input('Enter an FTC related word (eg. 3d): ')
+        print(semantics_search(vector_map, ui))
 
 # deletes unwanted characters
 def filter(text):
@@ -69,22 +66,18 @@ def check_frequency(target, pages):
         frequencies.append(op.countOf(page, target))
     return frequencies
 
-# vectorizes a list of words
-def vectorize_words(words, pages):
-    start = time.time()
-    vectors = []
-    for word in words:
-        vectors.append(check_frequency(word, pages))
-    end = time.time()
-    print('Seconds spent vectorizing:', round(end-start,4))
-    return vectors
-
-# creates hashmap where words serve as keys and vectors as data
-def create_hashmap(words, vectors):
+# vectorizes a list of words and places them in a hashmap
+def vectorize_words(unique_words, pages):
     vector_map = {}
-    for i, vector in enumerate(vectors):
-        vector_map[words[i]] = []
-        vector_map[words[i]] += vector
+    start = time.time()
+    for unique_word in unique_words:
+            vector_map[unique_word] = [0] * 101
+    for i, page in enumerate(pages):
+        for word in page:
+            if word in unique_words:
+                vector_map[word][i] += 1
+    end = time.time()
+    print(f"Vectorization finished in {round(end-start,3)} seconds")
     return vector_map
 
 # verifies if key is in hashmap to prevent KeyError and retrieved value if key is valid
@@ -105,9 +98,10 @@ def semantics_search(hashmap, word):
     # get the similarity value between the chosen word and every other word in the hashmap, if similarity is greater than 0 then add to the similarities array
     similarities = {}
     for key, value in hashmap.items():
-        if get_similarity(vector, value) > 0 and not key == word.lower():
+        similarity = get_similarity(vector, value)
+        if similarity > 0 and not check_contain(key, word):
             similarities[key] = 0
-            similarities[key] += get_similarity(vector, value)
+            similarities[key] += similarity
 
     # sort similarities in descending order so most similar words come first
     sorted = sort_hashmap_by_value(similarities)
@@ -120,6 +114,10 @@ def semantics_search(hashmap, word):
 # returns the cosine angle between two vectors
 def get_similarity(v1, v2):
     return numpy.dot(v1, v2) / (numpy.linalg.norm(v1) * numpy.linalg.norm(v2))
+
+# checks if two words contain eachother
+def check_contain(word_one, word_two):
+    return word_one in word_two or word_two in word_one
 
 # sorts hashmap via merge sort
 def sort_hashmap_by_value(hashmap):
